d925398102b9eb4df9a506586ef7468b37df3054
Noel Kelly <gnoll110@yahoo.com.au>
2014-03-03 17:47:27 +1000

    added aruba set to top of first spec

--- spec/video_stick_spec.rb
+++ spec/video_stick_spec.rb
@@ -1,3 +1,15 @@
+require 'aruba'
+require 'aruba/api'
+
+include Aruba::Api
+
+require 'pathname'
+
+root = Pathname.new(__FILE__).parent.parent
+
+# Allows us to run commands directly, without worrying about the CWD
+ENV['PATH'] = "#{root.join('bin').to_s}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
+
 require "video_stick"
 
 describe VideoStick::Builder do
