0013a28f7dde56bedc1235f249626c84b39caab0
Noel Kelly <gnoll110@yahoo.com.au>
2014-03-03 17:56:26 +1000

    added spec to test bare 'video_stick' CLI call

--- spec/video_stick_spec.rb
+++ spec/video_stick_spec.rb
@@ -21,3 +21,18 @@ describe VideoStick::Builder do
     VideoStick::Builder.name("Ruby").should eql("Whiz!")
   end
 end
+
+describe "video_stick" do
+  #it "should emit valid YAML to STDOUT" do
+  it "bare run" do
+    # Run the command with Aruba's run_simple helper
+    run_simple "video_stick"
+
+    # Test the YAML can be parsed
+    lambda {
+      userdata = YAML.parse(all_output)
+      userdata.should_not be_nil
+    }.should_not raise_error
+    assert_exit_status(0)
+  end
+end
